490912123d048c1203af7f7c3a3258bab327bb4f
Molorane Mothusi <molorane.mothusi@gmail.com>
2020-12-17 00:17:30 +0200

    app icon

--- app/src/main/java/com/blessy/com/bukaeakhale/ui/frag/book/BookFragment.java
+++ app/src/main/java/com/blessy/com/bukaeakhale/ui/frag/book/BookFragment.java
@@ -127,7 +127,6 @@ public class BookFragment extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 bookParam = ((TextView)view).getText().toString();
-                Toast.makeText(getActivity(),bookParam ,Toast.LENGTH_SHORT).show();
                 bookActivity.changeBook(bookParam);
                 viewPager.setCurrentItem(viewPager.getCurrentItem()+1); // Increment ViewPager's position
             }
